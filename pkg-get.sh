2f909ea02d0d61bff794290358282edf5520bc52
Suyash <suyashmadhesia@gmail.com>
2022-11-12 22:57:58 +0530

    setup script for windows

--- pkg-get.sh
+++ pkg-get.sh
@@ -1,5 +1,11 @@
 $(pwd)/scripts/env.sh
-source $(pwd)/env/bin/activate || $(pwd)/env/Scripts/activate
+SCR="$(pwd)/env/bin/activate"
+if [ -e "$SCR" ]
+then
+    source $(pwd)/env/bin/activate
+else
+    $(pwd)/env/Scripts/activate
+fi  
 RED='\033[0;31m'
 if test "$#" -eq 0
 then 
--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
-source $(pwd)/env/bin/activate || $(pwd)/env/Scripts/activate
+SCR="$(pwd)/env/bin/activate"
+if [ -e "$SCR" ]
+then
+    source $(pwd)/env/bin/activate
+else
+    $(pwd)/env/Scripts/activate
+fi 
 RED='\033[0;31m'
 if test "$#" -eq 0
 then 
